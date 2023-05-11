d005005d46b6303ea1fbaa091ffc5ffa7a0b8cea
adarsh-scanta <adarsh@scanta.io>
2023-05-12 03:19:26 +0530

    new build

--- next.config.js
+++ next.config.js
@@ -7,10 +7,10 @@ module.exports = withAntdLess({
   // optional https://github.com/webpack-contrib/css-loader#object
   // cssLoaderOptions: {
   //   // ...
-    mode: "local",
-    localIdentName: "[hash:base64:8]",
-    exportLocalsConvention: "camelCase",
-    exportOnlyLocals: false,
+  mode: "local",
+  localIdentName: "[hash:base64:8]",
+  exportLocalsConvention: "camelCase",
+  exportOnlyLocals: false,
   //   // ...
   //   getLocalIdent: (context, localIdentName, localName, options) => {
   //     return "whatever_random_class_name";
@@ -20,34 +20,10 @@ module.exports = withAntdLess({
   typescript: {
     ignoreBuildErrors: true,
   },
-
+  reactStrictMode: false,
   compiler: {
     // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
-    styledComponents:
-      true |
-      {
-        // Enabled by default in development, disabled in production to reduce file size,
-        // setting this will override the default for all environments.
-        //   displayName?: boolean,
-        // Enabled by default.
-        ssr: true,
-        // Enabled by default.
-        //   fileName?: boolean,
-        // Empty by default.
-        //   topLevelImportPaths?: string[],
-        // Defaults to ["index"].
-        //   meaninglessFileNames?: string[],
-        // Enabled by default.
-        //   cssProp?: boolean,
-        // Empty by default.
-        //   namespace?: string,
-        // Not supported yet.
-        //   minify?: boolean,
-        // Not supported yet.
-        //   transpileTemplateLiterals?: boolean,
-        // Not supported yet.
-        //   pure?: boolean,
-      },
+    styledComponents: true,
   },
 
   webpack(config) {
