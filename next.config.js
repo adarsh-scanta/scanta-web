8b28740e865f5bf4bcede5a545ed794a860db0ce
adarsh-scanta <adarsh@scanta.io>
2023-05-12 04:44:59 +0530

    test build2

--- next.config.js
+++ next.config.js
@@ -3,19 +3,20 @@ const withAntdLess = require("next-plugin-antd-less");
 
 module.exports = withAntdLess({
   // modifyVars: { '@primary-color': '#04f' }, // optional
+  lessVarsFilePath: "./src/styles/variables.less", // optional
   lessVarsFilePathAppendToEndOfContent: false, // optional
   // optional https://github.com/webpack-contrib/css-loader#object
-  // cssLoaderOptions: {
-  //   // ...
+  cssLoaderOptions: {
+    // ...
   mode: "local",
   localIdentName: "[hash:base64:8]",
   exportLocalsConvention: "camelCase",
   exportOnlyLocals: false,
-  //   // ...
-  //   getLocalIdent: (context, localIdentName, localName, options) => {
-  //     return "whatever_random_class_name";
-  //   },
-  // },
+    // ...
+    getLocalIdent: (context, localIdentName, localName, options) => {
+      return "whatever_random_class_name";
+    },
+  },
 
   typescript: {
     ignoreBuildErrors: true,
