f557aeafae24a5c48c0ff17e03c0656c10313240
adarsh-scanta <adarsh@scanta.io>
2023-06-02 12:53:31 +0530

    gtag updated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,27 +8,26 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <Script>{`(function (w, d, s, l, i) {
+        <script>{`(function (w, d, s, l, i) {
       w[l] = w[l] || []; w[l].push({
         'gtm.start':
           new Date().getTime(), event: 'gtm.js'
       }); var f = d.getElementsByTagName(s)[0],
         j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
           'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', 'GTM-5S97HQ4');`}</Script>
+    })(window, document, 'script', 'dataLayer', 'GTM-5S97HQ4');`}</script>
 
-        <Script
+        <script async
           src="https://www.googletagmanager.com/gtag/js?id=G-7KK004LHNR"
-          strategy="afterInteractive"
         />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <script >
           {` window.dataLayer = window.dataLayer || [];
     function gtag() { dataLayer.push(arguments); }
     gtag('js', new Date());
 
     gtag('config', 'G-7KK004LHNR');`}
-        </Script>
-        <Script>
+        </script>
+        <script>
           {`!function (f, b, e, v, n, t, s) {
       if (f.fbq) return; n = f.fbq = function () {
         n.callMethod ?
@@ -42,7 +41,7 @@ export default function App({ Component, pageProps }: AppProps) {
       'https://connect.facebook.net/en_US/fbevents.js');
     fbq('init', '885285045904955');
     fbq('track', 'PageView');`}
-        </Script>
+        </script>
         <noscript>
           <img
             height="1"
