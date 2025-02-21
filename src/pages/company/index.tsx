ee708f500beb0b1f2d26c882fac14a6596314ae3
adarsh-scanta <91716659+adarsh-scanta@users.noreply.github.com>
2025-02-21 16:01:42 +0530

    team removed

--- src/pages/company/index.tsx
+++ src/pages/company/index.tsx
@@ -42,7 +42,7 @@ export default function Home() {
       <Styles />
       <ScrollToTop />
       <MainBlock />
-      <StoryBlock />
+      {/* <StoryBlock /> */}
       {/* <AdvisorBlock /> */}
       <ContactBlock />
       <Newsletter />
